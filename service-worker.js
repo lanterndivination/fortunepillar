40cc49a6d4918b28082515f2162d750bfd18f29b
lanterndivination <lantern.divination@gmail.com>
2026-01-08 17:22:11 +0900

    feat: Implement PWA support (manifest, icons, service worker)

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'fortune-pillar-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './icon-192.png',
+  './icon-512.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
